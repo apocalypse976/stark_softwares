aa0e60193236f7d7d431fbca0091ade682b25a44
apocalypse976 <166110377+apocalypse976@users.noreply.github.com>
2024-11-07 17:33:36 +0530

    updated

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -6,3 +6,58 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+var whyslider= $('.why-slider')
+whyslider.owlCarousel({
+    items:4,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:100,
+    autoplayHoverPause:false,
+    margin:10,
+    responsiveClass:true,
+    dots:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
+var servicesslider= $('.servicesslider')
+servicesslider.owlCarousel({
+    items:4,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:100,
+    autoplayHoverPause:false,
+    margin:10,
+    responsiveClass:true,
+    dots:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
