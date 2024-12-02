e215ee7409a9eb0f8711f292a17e859aeb546e3d
apocalypse976 <166110377+apocalypse976@users.noreply.github.com>
2024-12-02 13:30:03 +0530

    updated

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -53,3 +53,31 @@ servicesslider.owlCarousel({
         }
     }
 });
+
+var catalougeslider= $('.catalaugeacoursel')
+catalougeslider.owlCarousel({
+    items:4,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:100,
+    autoplayHoverPause:false,
+    margin:10,
+    responsiveClass:true,
+    dots:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
